8e5667380b92c4d29709d73beb952108fc8660b7
mykle jones <mykle.j91@gmail.com>
2020-01-21 01:23:50 -0500

    edit good

--- src/components/editUserForm.js
+++ src/components/editUserForm.js
@@ -5,12 +5,13 @@ import {Form, Header, Button, FormInput } from 'semantic-ui-react'
 
 function EditUserForm(props){
 
-  const {userData} = useSelector(state=>state)
+  const {userData, uLoaded} = useSelector(state=>state)
   const dispatch = useDispatch()
-  const [email, setEmail] = useState("")
-  const [lastName, setLastName] = useState("")
-  const [firstName, setFirstName] = useState("")
-  const [username, setUserName] = useState("")
+  const [email, setEmail] = useState(userData.email)
+  let name = userData.name.split(' ')
+  const [lastName, setLastName] = useState(name[1])
+  const [firstName, setFirstName] = useState(name[0])
+  const [username, setUserName] = useState(userData.username)
   const [age, setAge] = useState(userData.age)
   const [dob, setDob] = useState(userData.dob)
   const [interest, setInterest] = useState(userData.interest)
@@ -19,8 +20,7 @@ function EditUserForm(props){
   const [preference, setPreference] = useState(userData.preference)  
   const [bodytype , setBodyType] = useState(userData.body_type)  
   const [profession, setProfession] = useState(userData.profession)  
-  
-
+  const [cityState, setCityState] = useState(userData.city_state)
 
   const options = [
     { key: 'm', text: 'Male', value: 'male' },
@@ -44,11 +44,35 @@ function EditUserForm(props){
     { key: 'h', text: 'Husky', value: 'husky' },
     
   ]
+  const options2 = [
+    { key: 'f', text: 'Friends', value: 'friends' },
+    { key: 'd', text: 'Dating', value: 'dating' },
+    { key: 'r', text: 'Relationship', value: 'relationship' },
+  ]
   const editFormSubmit = evt =>{
     evt.preventDefault()
-    console.log(evt)
-  }
+    let nameInput = firstName+" "+lastName
 
+    let updateInfo = {
+      id:userData.id,
+      name: nameInput,
+      username: username,
+      email: email,
+      age: age,
+      dob: dob,
+      sex: sex,
+      city_state: cityState,
+      interest: interest,
+      about_me: aboutMe,
+      preference: preference,
+      body_type:bodytype, 
+      profession: profession 
+
+    }
+    dispatch(actionCreator.editUserProfile(updateInfo, localStorage.token, localStorage.loggedInUserId))
+    
+  }
+console.log(lastName)
   const FormInput = evt =>{
     switch(evt.target.name){
       case 'First Name':
@@ -86,12 +110,17 @@ function EditUserForm(props){
       case 'profession':
         setProfession(evt.target.value)
         break; 
+      case 'citystate':
+        setCityState(evt.target.value)
+        break;
     }
   }
   
 
 return(
 <>
+  
+{ }
 <Header as="h1">Edit</Header>
 <Form onSubmit={editFormSubmit} >
 <Form.Group widths='equal'>
@@ -99,7 +128,7 @@ return(
               fluid
               label='First Name'
               name='First Name'
-              placeholder='First Name'
+              placeholder={name[0]}
               type='text'
               onChange={FormInput}
               />
@@ -107,27 +136,35 @@ return(
               fluid
               label='Last Name'
               name='Last Name'
-              placeholder='Last Name'
+              placeholder={name[1]}
               type='text'
+              onChange={FormInput}
               />
             </Form.Group>
-  <Form.Input  label='Username' name='username' placeholder={userData.username} />
-  <Form.Input  label='Email' name='email' placeholder={userData.email} />
+  <Form.Input  label='Username' name='username' placeholder={userData.username}   onChange={FormInput} />
+  <Form.Input  label='Email' onChange={FormInput}  name='email' placeholder={userData.email} />
   {/* <Form.Input  label='Photo'  /> */}
-  <Form.Input  label='Age' name='age' placeholder={userData.age} />
-  <Form.Input  label='Dob' name='dob' placeholder={userData.dob} />
-  <Form.Input  label='City,State' placeholder={userData.city_state} />
+  <Form.Input  label='Age' name='age' placeholder={userData.age} onChange={FormInput} />
+  <Form.Input  label='Dob' name='dob' placeholder={userData.dob} onChange={FormInput} />
+  <Form.Input  label='City,State' name='citystate' placeholder={userData.city_state} onChange={FormInput} />
   <Form.Select
             fluid
             label='Sex'
+            onChange={FormInput}
             name='sex'
             options={options1}
-            placeholder='Gender'
+            placeholder={userData.sex}
           />
-  <Form.Input  label='About me' name='about_me' placeholder={userData.about_me} />
-  <Form.Input  label='Interest' name='interest' placeholder={userData.interest} />
+  <Form.Input  label='About me' name='about_me' placeholder={userData.about_me} onChange={FormInput} />
+  <Form.Select
+    label='Interest' 
+    name='interest'
+    options={options2}
+    placeholder={userData.interest} 
+    onChange={FormInput} />
   <Form.Select
             fluid
+            onChange={FormInput}
             label='Preference'
             name='preference'
             options={options}
@@ -135,13 +172,14 @@ return(
           />
    <Form.Select
             fluid
+            onChange={FormInput}
             name='body_type'
             label='Body Type'
             options={bodyType}
-            placeholder='Gender'
+            placeholder={userData.body_type}
           />
 
-  <Form.Input  label='Profession' name='profession' placeholder={userData.profession} />
+  <Form.Input onChange={FormInput} label='Profession' name='profession' placeholder={userData.profession} />
   <Button basic color='blue'type='submit' >Update</Button>
 </Form>
 
